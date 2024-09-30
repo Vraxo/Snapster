70e626c6b51594f7316978b53fbeb6a2a58a815e
Vraxo <pkafsharix@gmail.com>
2024-09-30 16:58:41 +0330

    Made namespaces flexible in scene

--- Snapster/Source/A - Nodex/Core/Scene.cs
+++ Snapster/Source/A - Nodex/Core/Scene.cs
@@ -16,9 +16,7 @@ public class Scene
         T instance = new();
 
         string[] fileLines = File.ReadAllLines(path);
-
         object obj = null;
-
         bool firstNode = true;
 
         foreach (string line in fileLines)
@@ -26,12 +24,18 @@ public class Scene
             if (line.StartsWith('[') && line.EndsWith(']'))
             {
                 string typeName = line.Substring(1, line.Length - 2).Trim();
-                Type type = Type.GetType("Snapster." + typeName);
+                Type type = ResolveType(typeName); // Use the new method to resolve the type
+
+                if (type == null)
+                {
+                    throw new Exception($"Type '{typeName}' could not be found.");
+                }
+
                 obj = Activator.CreateInstance(type);
 
                 if (firstNode)
                 {
-                    instance = (T?)obj;
+                    instance = (T)obj;
                     firstNode = false;
                 }
                 else
@@ -55,6 +59,34 @@ public class Scene
         }
 
         return instance;
+    }
+
+    private Type ResolveType(string typeName)
+    {
+        // Get all loaded assemblies
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+        foreach (var assembly in assemblies)
+        {
+            // Try to find the type in the current assembly
+            var type = assembly.GetType(typeName, false, true);
+            if (type != null)
+            {
+                return type;
+            }
+
+            // Additionally, you could try to find the type with a default namespace if applicable
+            var defaultNamespace = assembly.GetName().Name; // Get the default namespace
+            var namespacedTypeName = defaultNamespace + "." + typeName;
+            type = assembly.GetType(namespacedTypeName, false, true);
+            if (type != null)
+            {
+                return type;
+            }
+        }
+
+        // If no type found, return null
+        return null;
     }
 
     private static void SetValue(object obj, string name, object value)
@@ -82,6 +114,7 @@ public class Scene
 
         if (propertyInfo != null && propertyInfo.CanWrite)
         {
+            // Handle Vector2 property
             if (propertyInfo.PropertyType == typeof(Vector2))
             {
                 string[] tokens = value.ToString().Split(' ');
@@ -93,6 +126,7 @@ public class Scene
                 return;
             }
 
+            // Handle Enum property
             if (propertyInfo.PropertyType.IsEnum)
             {
                 var enumValue = Enum.Parse(propertyInfo.PropertyType, value.ToString());
@@ -100,6 +134,7 @@ public class Scene
                 return;
             }
 
+            // Handle other property types
             if (propertyInfo.PropertyType == typeof(int))
             {
                 propertyInfo.SetValue(obj, int.Parse(value.ToString()));
@@ -121,6 +156,19 @@ public class Scene
             if (propertyInfo.PropertyType == typeof(bool))
             {
                 propertyInfo.SetValue(obj, bool.Parse(value.ToString()));
+                return;
+            }
+
+            if (propertyInfo.PropertyType == typeof(string))
+            {
+                string stringValue = value.ToString();
+
+                if (stringValue.Length >= 2 && stringValue.StartsWith("\"") && stringValue.EndsWith("\""))
+                {
+                    stringValue = stringValue[1..^1];
+                }
+
+                propertyInfo.SetValue(obj, stringValue);
                 return;
             }
 
--- Snapster/Source/ImageDisplayer.cs
+++ Snapster/Source/ImageDisplayer.cs
@@ -12,21 +12,12 @@ public partial class ImageDisplayer : AspectRatioContainer
 
     public override void Update()
     {
-        Size = Screen.Size;
-
-        //Position = Screen.Size / 2;
-        //
-        //Vector2 center = Screen.Size / 2;
-        //
-        //Vector2 textureSize = new Vector2(Texture.Width, Texture.Height);
-        //
-        //Vector2 totalRatio = Screen.Size / textureSize;
-        //float ratio = totalRatio.X < totalRatio.Y ? totalRatio.X : totalRatio.Y;
-        //
-        //Vector2 newSize = textureSize * ratio;
-        //
-        //Size = newSize;
-
+        UpdateSize();
         base.Update();
+    }
+
+    private void UpdateSize()
+    {
+        Size = Screen.Size;
     }
 }
